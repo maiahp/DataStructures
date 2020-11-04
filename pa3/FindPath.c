6930ec0f8f526fb48366be887d2b24a8ead88da4
mapardo <mapardo@ucsc.edu>
2020-11-03 23:16:23 -0800

    update

--- pa3/FindPath.c
+++ pa3/FindPath.c
@@ -12,7 +12,6 @@
 int main(int argc, const char * argv[]) {
     FILE *in, *out;
 
-    /*
     if ( argc != 3 ) {
         fprintf(stderr, "Usage: %s <input file> <outputfile>\n", argv[0]);
         exit(1);
@@ -28,9 +27,8 @@ int main(int argc, const char * argv[]) {
         printf("Unable to open file %s for writing\n", argv[2]);
         exit(1);
     }
-   */
     
-    
+    /*
     // hardcoding the filename (testing):
     in = fopen("in2.txt", "r");
     if(in==NULL) {
@@ -43,7 +41,7 @@ int main(int argc, const char * argv[]) {
         exit(1);
     }
     // end (testing)
-     
+    */
     
     int n;
     
