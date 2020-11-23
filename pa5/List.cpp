9f226c75034439d3bdb554a1f2dadd8e56b341a4
mapardo <mapardo@ucsc.edu>
2020-11-23 14:34:55 -0800

    finished and tested List.cpp

--- pa5/List.cpp
+++ pa5/List.cpp
@@ -177,7 +177,11 @@ int List::moveNext() {
         cerr << "List Error: calling moveNext() when there is no next element\n";
         exit(1);
     }
-    
+    int data = this->afterCursor->data;   // save data of after cursor to return
+    this->beforeCursor = this->beforeCursor->next;
+    this->afterCursor = this->afterCursor->next;
+    this->pos_cursor++;
+    return data;
 }
 
 // movePrev()
@@ -189,14 +193,25 @@ int List::movePrev() {
         cerr << "List Error: calling movePrev() when there is no previous element\n";
         exit(1);
     }
-    
-    
+    int data = beforeCursor->data;         // data to return
+    this->beforeCursor = this->beforeCursor->prev;
+    this->afterCursor = this->afterCursor->prev;
+    this->pos_cursor--;
+    return data;
 }
 
 // insertAfter()
 // Inserts x after cursor.
 void List::insertAfter(int x) {
     // Note: X BECOMES AFTER CURSOR
+    Node* temp = new Node(x);
+    this->beforeCursor->next = temp;
+    this->afterCursor->prev = temp;
+    temp->prev = beforeCursor;
+    temp->next = afterCursor;
+
+    this->afterCursor = temp; // afterCursor is updated to point to new node
+    this->num_elements++;
 }
 
 // insertBefore()
@@ -204,6 +219,17 @@ void List::insertAfter(int x) {
 void List::insertBefore(int x) {
     // Note: X BECOMES BEFORE CURSOR
     // the cursor must be moved forward, in order to make x the beforeCursor
+    Node *temp = new Node(x);
+    this->beforeCursor->next = temp;
+    this->afterCursor->prev = temp;
+    temp->prev = beforeCursor;
+    temp->next = afterCursor;
+
+    this->beforeCursor = temp;
+    this->num_elements++;
+    
+    // move cursor to next place (afterCursor doesn't change)
+    this->pos_cursor++;
 }
 
 // eraseAfter()
@@ -214,7 +240,13 @@ void List::eraseAfter() {
         cerr << "List Error: calling eraseAfter() when cursor is at end of list\n";
         exit(1);
     }
-    
+    Node* nodeToDelete = this->afterCursor;
+    this->beforeCursor->next = this->afterCursor->next;
+    this->afterCursor->next->prev = this->beforeCursor;
+    this->afterCursor = this->afterCursor->next; // update afterCursor
+    delete nodeToDelete;
+    this->num_elements--;
+    // cursor is at correct position, don't update
 }
 
 // eraseBefore()
@@ -225,6 +257,17 @@ void List::eraseBefore() {
         cerr << "List Error: calling eraseBefore() when cursor is at front of list\n";
         exit(1);
     }
+    // note:
+    // to delete element before the cursor
+    // the cursor must move one backwards
+    // its afterCursor stays the same element
+    Node* nodeToDelete = this->beforeCursor;
+    this->afterCursor->prev = this->beforeCursor->prev;
+    this->beforeCursor->prev->next = this->afterCursor;
+    this->beforeCursor = this->beforeCursor->prev; // update beforeCursor
+    this->pos_cursor--; // update cursor to be at correct place
+    delete nodeToDelete;
+    this->num_elements--;
 }
 
 // findNext()
@@ -236,6 +279,16 @@ void List::eraseBefore() {
 // and returns -1.
 int List::findNext(int x) {
     // start from current cursor position
+    while (this->position() < this->size()-1) { // while cursor is not at backDummy
+        if (this->afterCursor->data == x) { // compare x with elem after the cursor
+            this->moveNext(); // if x found, place cursor after x
+            return(this->position());
+        }
+        this->moveNext(); // x is not found, move cursor forward
+    }
+    // if while exits, we have not found x
+    this->moveBack();
+    return(-1);
 }
 
 // findPrev()
@@ -247,6 +300,16 @@ int List::findNext(int x) {
 // returns -1.
 int List::findPrev(int x) {
     // start from current cursor position
+    while (this->position() > 0) {
+        if (this->beforeCursor->data == x) {
+            this->movePrev();
+            return(this->position());
+        }
+        this->movePrev(); // x not found, move cursor forward
+    }
+    // if while exits, we have not found x
+    this->moveFront();
+    return(-1);
 }
 
 // cleanup()
@@ -258,6 +321,31 @@ int List::findPrev(int x) {
 // did before cleanup() was called.
 void List::cleanup() {
     // start at front of list
+    Node* i = this->frontDummy->next;
+    Node* j;
+    while (i != this->backDummy) {
+        j = i->next;
+        while (j != this->backDummy) {
+            if (i->data == j->data) { // datas are the same
+                Node* temp = j;
+                // delete j, it is the duplicate
+                j->prev->next = j->next;
+                j->next->prev = j->prev;
+                delete temp;
+                temp = nullptr;
+                this->num_elements--;
+                
+                if (pos_cursor > num_elements) {
+                    // if pos_cursor is out of range after deletion
+                    // then we ammend the cursor to be at back position
+                    this->moveBack();
+                }
+                break;
+            }
+            j = j->next;
+        }
+        i = i->next;
+    }
 }
 
 // clear()
@@ -276,6 +364,27 @@ void List::clear() {
 // the elements of L. The returned List's cursor will be at its front
 // (position 0).
 List List::concat(const List& L) {
+    List result;
+    
+    // traverse "this"
+    Node* curr = this->frontDummy->next;
+    while(curr != this->backDummy) {
+        result.insertAfter(curr->data);
+        
+        result.moveNext();
+        curr = curr->next;
+    }
+    
+    // traverse L
+    Node* currL = L.frontDummy->next;
+    while(currL != L.backDummy) {
+        result.insertAfter(currL->data);
+        result.moveNext();
+        currL = currL->next;
+    }
+    
+    result.moveFront(); // update result's cursor to front
+    return(result);
 }
 
 
@@ -285,12 +394,41 @@ List List::concat(const List& L) {
 // Returns a string representation of this List consisting of a comma
 // separated sequence of elements, surrounded by parentheses.
 string List::to_string() {
+    // don't use cursor to traverse, we want to preserve cursor
+    string s = "(";
+    int count = 1;
+    Node* curr = this->frontDummy->next;
+    while (curr != this->backDummy) {
+        s += std::to_string(curr->data);
+        if (count < num_elements) {
+            s+= ", ";
+            count++;
+        }
+        curr = curr->next;
+    }
+    s += ")";
+    return s;
 }
 
 // equals()
 // Returns true if and only if this List is the same integer sequence as R.
 // The cursors in this List and in R are unchanged.
 bool List::equals(const List& R) {
+    bool eq = false;
+    Node* N = nullptr;
+    Node* M = nullptr;
+
+    eq = (this->num_elements == R.num_elements);
+    
+    N = this->frontDummy->next;
+    M = R.frontDummy->next;
+    
+    while(eq && N != nullptr){
+        eq = (N->data == M->data);
+        N = N->next;
+        M = M->next;
+    }
+    return eq;
 }
 
 
@@ -298,17 +436,39 @@ bool List::equals(const List& R) {
 
 // operator<<()
 // Inserts string representation of L into stream.
-std::ostream& operator<<( std::ostream& stream, List& L ) {
+std::ostream& operator<<( std::ostream& stream, List& L ) { // i removed 'friend' here make sure this is ok
+        return stream << L.List::to_string();
+        //return stream << L.List::to_string();
 }
 
 // operator==()
 // Returns true if and only if A is the same integer sequence as B. The
 // cursors in both Lists are unchanged are unchanged.
-bool operator==( List& A, const List& B ) {
+bool operator==( List& A, const List& B ) { // i removed 'friend' here make sure this is ok
+    return A.List::equals(B);
 }
 
 // operator=()
 // Overwrites the state of this List with state of L.
 List& List::operator=( const List& L ) {
+    if( this != &L ){ // not self assignment
+        this->clear();
+        
+        Node* currL = L.frontDummy->next;
+        while(currL != L.backDummy) {
+            this->insertAfter(currL->data);
+            this->moveNext();
+            currL = currL->next;
+        }
+        
+        // cursor of L should be cursor of this
+        this->pos_cursor = L.pos_cursor;
+        this->beforeCursor = L.beforeCursor;
+        this->afterCursor = L.afterCursor;
+    }
 
-};
+    // return this with the new data installed
+    return *this;
+
+    // the copy, if there is one, is deleted upon return
+}
--- pa5/ListTest.cpp
+++ pa5/ListTest.cpp
@@ -4,3 +4,221 @@
 * ListTest.h
 * Test module for the List ADT
 */
+#include <iostream>
+#include <string>
+#include "List.h"
+
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    cout << "Testing List" << endl << endl;
+    cout << "Creating List l" << endl << endl;
+    
+    List l, A, B, C;
+    
+    int count = 1;
+    l.moveFront();
+    while(count <= 10) {
+        l.insertBefore(count);
+        count++;
+        l.moveNext();
+    }
+    cout << "List l is: " << endl;
+    cout << l << endl << endl;
+    cout << l << endl;
+    
+    // testing peekNext() and moveNext()
+    cout << "Testing peekNext() and moveNext()" << endl;
+    l.moveFront();
+    while(l.position() < l.size()) {
+        int data = l.peekNext();
+        cout<< data << endl;
+        l.moveNext();
+    }
+    cout<<endl<<endl<<endl;
+    
+    
+    // testing peekPrev()
+    cout << "Testing peekNext() and moveNext()" << endl;
+    l.moveBack();
+    while(l.position() > 0) {
+        int data = l.peekPrev();
+        cout<< data << endl;
+        l.movePrev();
+    }
+    cout<<endl<<endl<<endl;
+    
+    
+    // testing eraseAfter
+    cout << "Moving cursor to position 3" << endl;
+    cout << "Calling eraseAfter()" << endl;
+    l.moveFront();
+    for(int i=1; i<=3; i++) {
+        l.moveNext();
+    }
+    l.eraseAfter();
+    cout<< "List l is: " << endl;
+    cout << l << endl;
+    
+    cout<<endl<<endl;
+    
+    
+    // testing eraseBefore
+    cout<<"Moving cursor to position 7" << endl;
+    cout << "Calling eraseBefore()" << endl;
+    l.moveFront();
+    for(int i=1; i<=7; i++) {
+        l.moveNext();
+    }
+    l.eraseBefore();
+    cout<< "List l is: " << endl;
+    cout << l << endl;
+    
+    cout<<endl<<endl;
+    
+    
+    // testing findNext
+    cout<< "Testing findNext() and eraseBefore()" << endl;
+    cout<< "Creating List A" << endl << endl;
+    
+    count = 1;
+    A.moveFront();
+    while(count <= 10) {
+        A.insertBefore(count);
+        count++;
+        A.moveNext();
+    }
+    A.insertAfter(5);
+    A.moveNext();
+    A.insertAfter(3);
+    A.moveNext();
+    A.insertAfter(2);
+    
+    cout << "List A is: " << endl;
+    cout << A << endl << endl;
+    
+    cout << "Calling findNext() with integer 5" << endl;
+    A.moveFront();
+    A.findNext(5);
+    cout<< "Cursor is at position " << A.position() << endl;
+    cout<< "Calling eraseBefore() to delete element with integer 5"<< endl << endl;
+    A.eraseBefore();
+    
+    cout << "List A is: " << endl;
+    cout << A << endl;
+
+    cout<<endl<<endl;
+    
+    
+    // testing findPrev
+    cout<< "Testing findPrev() and eraseAfter()" << endl;
+    
+    cout << "List A is: " << endl;
+    cout << A << endl << endl;
+    
+    cout << "Calling findPrev() with integer 10" << endl;
+    A.moveBack();
+    A.findPrev(10);
+    cout<< "Cursor is at position " << A.position() << endl;
+    cout<< "Calling eraseAfter() to delete element with integer 10"<< endl << endl;
+    A.eraseAfter();
+    
+    cout << "List A is: " << endl;
+    cout << A << endl;
+
+    cout<<endl<<endl;
+    
+    
+    // testing clean up
+    cout << "Inserting into A" << endl;
+    A.moveFront();
+    
+    A.insertAfter(10);
+    
+    A.moveNext();
+    A.moveNext();
+    
+    A.insertAfter(10);
+    
+    A.moveNext();
+    A.moveNext();
+    A.moveNext();
+    A.moveNext();
+    A.moveNext();
+    
+    A.insertAfter(10);
+    A.insertAfter(3);
+    A.insertAfter(5);
+    A.insertAfter(5);
+    
+    cout << "List A is: " << endl;
+    cout << A << endl << endl;
+    
+    cout << "Calling cleanup()" <<endl << endl;
+    A.cleanup();
+    
+    cout << "List A is: " << endl;
+    cout << A << endl;
+    
+    cout<<endl<<endl<<endl;
+    
+    
+    // testing clear()
+    cout << "Calling clear() on list A" << endl << endl;
+    A.clear();
+    cout << "List A is: " << endl;
+    cout << A << endl;
+    
+    cout<<endl<<endl<<endl;
+    
+    
+    // testing concat
+    cout << "Inserting into A" << endl;
+    
+    count = 1;
+    A.moveFront();
+    while(count <= 7) {
+        A.insertBefore(count);
+        count++;
+        A.moveNext();
+    }
+    
+    cout << "List A is: " << endl;
+    cout << A << endl;
+    
+    cout << "Calling concat() to concat list A and list l" << endl << endl;
+    B = A.concat(l);
+    
+    cout << "The concatenated list B is: " << endl;
+    cout << B << endl;
+    
+    cout<<endl<<endl<<endl;
+    
+    
+    // testing equals() and copy()
+    cout << "Testing equals(), copy(), operator==() and operator=()" << endl;
+    cout << "Creating list C which is a copy of A" << endl;
+    C = A;
+    cout << "List C is: " << endl;
+    cout << C << endl;
+    
+    
+    cout << "List l and A are the same: ";
+    if (l == A) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    
+    cout << "List A and C are the same: ";
+    if (C == A) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    
+    cout<<endl<<endl<<endl;
+
+    return 0;
+}
