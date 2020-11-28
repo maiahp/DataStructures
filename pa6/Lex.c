850dd7c1d7fd408b2af18e39d647c582f51270e2
mapardo <mapardo@ucsc.edu>
2020-11-28 13:17:31 -0800

    first update to pa6

---
+++ pa6/Lex.c
@@ -0,0 +1,16 @@
+/*
+* Maiah Pardo, mapardo
+* 2020 Fall CSE 101 PA6
+* Lex.c
+* Client module for the Dictionary ADT
+*/
+
+
+#include <stdio.h>
+#include "Dictionary.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
