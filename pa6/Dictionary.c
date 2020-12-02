098ae3d1064133ad73034dc6debae60d3a6a5c1b
mapardo <mapardo@ucsc.edu>
2020-12-02 14:45:53 -0800

    fixed header comment detail

--- pa6/Dictionary.c
+++ pa6/Dictionary.c
@@ -1,7 +1,7 @@
 /*
 * Maiah Pardo, mapardo
-* 2020 Fall CSE 101 PA5
-* List.cpp
+* 2020 Fall CSE 101 PA6
+* Dictionary.c
 * Implementation file for Dictionary ADT
 * The Dictionary ADT is an underlying Binary Search Tree. A Dictionary maintains a set of pairs called key and value. The Dictionary contains a switch that determines whether or not it will accept duplicate keys.
 */
