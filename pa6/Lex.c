dc3440ae0cb0d17b8cb39c7092b3cba800032809
mapardo <mapardo@ucsc.edu>
2020-12-02 11:51:54 -0800

    update to Lex.c

--- pa6/Lex.c
+++ pa6/Lex.c
@@ -5,135 +5,91 @@
 * Client module for the Dictionary ADT
 */
 
-/*
 #include <stdio.h>
+#include <string.h>
 #include "Dictionary.h"
+#define MAX_LEN 300
+
 
 int main(int argc, const char * argv[]) {
-    Dictionary D = newDictionary(1);
-    insert(D, "hallo", 1);
-    insert(D, "there", 2);
-    insert(D, "abc", 3);
-    insert(D, "def", 4);
+
+    FILE *in, *out;
+
+    // check command line for correct number of arguments
+    // argv[0] = Lex, argv[1] = in file, argv[2] = out file
     
-    lookup(D, "hallo");
+    if ( argc != 3 ) {
+        fprintf(stderr, "Usage: %s <input file> <outputfile>\n", argv[0]);
+        exit(1);
+    }
+    // open files for reading and writing
+    in = fopen(argv[1], "r");
+    if(in==NULL) {
+        printf("Unable to open file %s for reading\n", argv[1]);
+        exit(1);
+    }
+    out = fopen(argv[2], "w");
+    if( out==NULL ){
+        printf("Unable to open file %s for writing\n", argv[2]);
+        exit(1);
+    }
     
-    delete(D, "hallo");
-    delete(D, "abc");
-    delete(D, "def");
-    delete(D, "there");
-    // there is an issue when deleting "there"
-    // for some reason, when D->root = D->NIL
-    // D->root's parent is set to the "there" node
-    // and still exists in allocated space AFTER "there" node is deleted
+    /*
+    // hardcoding the filename (testing):
+    in = fopen("in1.txt", "r");
+    if(in==NULL) {
+        printf("Unable to open file %s for reading\n", "input.txt"); // print error message to stderr
+        exit(1);
+    }
+    out = fopen("out1.txt", "w");
+    if(out==NULL) {
+        printf("Unable to open file %s for writing\n", "output.txt"); // print error message to stderr
+        exit(1);
+    }
+    // end (testing)
+    */
+    
+    // grab each line from file and insert key, val into dict
+    // line = key
+    // line number = val
+    
+    Dictionary D = newDictionary(0); // create a non-unique dictionary
+    int line_count = 1; // start at 1 so first line is line 1
+    char *line = NULL;
+    line = (char *)malloc(MAX_LEN);
+    
+    while(fgets(line, MAX_LEN, in) != NULL) {
+        int line_length = (int)strlen(line);
+        
+        if (line[line_length-1] == '\n') {  // if last char of line is '\n'
+            line[line_length-1] = '\0'; // replace '\n' with null terminator
+        } else {  // if no '\n' add a null terminator at end of line
+            line[line_length] = '\0';
+        }
+        
+        insert(D, line, line_count); // insert key = line, val = line_count into D
+        line_count++;
+    }
+    // when getline is finished, it returns 0 (false) and while exits
+    
+    fclose(in);
+    free(line);
+    
+    // print out the keys in order
+    int numKeys = size(D);
+    beginForward(D);
+    
+    for(int i=0; i < numKeys; i++) {
+        fprintf(out, "%s", (char*)currentKey(D));
+        if (i != numKeys-1) {   // if not at last print
+            fprintf(out, "\n");
+        }
+        next(D);
+    }
+    
     freeDictionary(&D);
+    fclose(out);
     
     return 0;
-}
-*/
-
-//-----------------------------------------------------------------------------
-// DictionaryClient.c
-// Test client for Dictionary ADT
-//-----------------------------------------------------------------------------
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include"Dictionary.h"
-
-int main(int argc, char* argv[]){
-   
-   int i, n=26;
-   Dictionary A = newDictionary(0);
-   Dictionary B = newDictionary(1);
-   char* word[] = { "n","z","w","k","i","c","l","d","t","a",
-                    "e","y","b","h","v","f","s","m","x","r",
-                    "o","u","p","g","j","q" };
-   VAL_TYPE x;
-   VAL_TYPE y;
-
-   // add pairs to A --------------------------------------
-   for(i=0; i<n; i++){
-      insert(A, word[i], i);
-   }
-
-   // add pairs to B
-   for(i=n-1; i>=0; i--){
-      insert(B, word[i], i);
-   }
-
-
-   // forward iteration over A ----------------------------
-   printf("forward A:\n");
-   for(x=beginForward(A); currentVal(A)!=VAL_UNDEF; x=next(A)){
-      printf("key: "KEY_FORMAT" value: "VAL_FORMAT"\n", currentKey(A), x);
-   }
-   printf("\n\n");
-
-   // reverse iteratation over B
-   printf("reverse B:\n");
-   for(y=beginReverse(B); currentVal(B)!=VAL_UNDEF; y=prev(B)){
-      printf("key: "KEY_FORMAT" value: "VAL_FORMAT"\n", currentKey(B), y);
-   }
-   printf("\n\n");
-
-
-   // print Dictionary A ----------------------------------
-   printf("Dictionary A (%sunique keys):\n", (getUnique(A)?"":"non-"));
-   printDictionary(stdout, A);
-   printf("\n");
-
-   // print Dictionary B
-   printf("Dictionary B (%sunique keys):\n", (getUnique(B)?"":"non-"));
-   printDictionary(stdout, B);
-   printf("\n");
-
-
-   // delete keys from A ----------------------------------
-   for(i=0; i<n; i+=2){
-      delete(A, word[i]);
-   }
-
-   // print Dictionary A after deletions
-   printf("Dictionary A (%sunique keys):\n", (getUnique(A)?"":"non-"));
-   printf("after deletions:\n");
-   printDictionary(stdout, A);
-   printf("\n");
-
-
-   // delete keys from B ----------------------------------
-   for(i=1; i<n; i+=2){
-      delete(B, word[i]);
-   }
-
-   // print Dictionary B after deletions
-   printf("Dictionary B (%sunique keys):\n", (getUnique(B)?"":"non-"));
-   printf("after deletions:\n");
-   printDictionary(stdout, B);
-   printf("\n");
-
-
-   // insert duplicates into A ----------------------------
-   for(i=1; i<n; i+=2){
-      insert(A, word[i], i);
-   }
-
-   // print Dictionary A after insertions
-   printf("Dictionary A (%sunique keys):\n", (getUnique(A)?"":"non-"));
-   printf("after insertions:\n");
-   printDictionary(stdout, A);
-   printf("\n");
-
-   // uncomment line below to get error message
-   // insert(B, word[10], 10);
-   // Dictionary Error:
-   // calling insert() on existing key: e
-
-
-   // free heap memory ------------------------------------
-   freeDictionary(&A);
-   freeDictionary(&B);
-
-   return(0);
 }
 
